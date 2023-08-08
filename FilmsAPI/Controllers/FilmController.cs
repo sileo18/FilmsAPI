5cd3f4a9b52d5c2799ba9da11656d0cdb6098728
unknown <lucastoledo358@gmail.com>
2023-08-08 01:23:35 -0300

    API Documented

--- FilmsAPI/Controllers/FilmController.cs
+++ FilmsAPI/Controllers/FilmController.cs
@@ -20,6 +20,13 @@ namespace FilmsAPI.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
+
         [HttpPost]
         public IActionResult AddFilm([FromBody] CreateFilmDTO filmDto)
         {
@@ -30,12 +37,26 @@ namespace FilmsAPI.Controllers
             return CreatedAtAction(nameof(FindById), new { id = film.Id }, film);
         }
 
+        /// <summary>
+        /// Lê os filmes do banco de dados
+        /// </summary>
+        /// <param name="skip">Parametros necessários para informar quantos filmes quer pular</param>
+        /// <param name="take">Parametros necessários para informar quantos filmes quer retornar</param>
+        /// <returns>IEnumerable</returns>
+        /// <response code="200">Caso inserção seja feita com sucesso</response>
         [HttpGet]
-        public IEnumerable<FilmModel> ReadFilms([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        public IEnumerable<ReadFilmDto> ReadFilms([FromQuery] int skip = 0, [FromQuery] int take = 50)
         {
-            return _context.Films.Skip(skip).Take(take).ToList();
+            return _mapper.Map<List<ReadFilmDto>>(_context.Films.Skip(skip).Take(take).ToList());
         }
 
+
+        /// <summary>
+        /// Lê os filmes do banco de dados a partir do Id
+        /// </summary>
+        /// <param name="id">Parametros necessários o retorno do filme pelo Id</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso o filme seja encontrado</response>
         [HttpGet("{id}")]
         public IActionResult FindById(int id)
         {
@@ -45,10 +66,17 @@ namespace FilmsAPI.Controllers
             {
                 return NotFound("Film not found");
             }
-
+            var filmDto = _mapper.Map<ReadFilmDto>(filmReturned);
             return Ok(filmReturned);
         }
 
+
+        /// <summary>
+        /// Atualiza o filme completo pelo Id
+        /// </summary>
+        /// <param name="id">Parametros necessários a atualização do filme pelo Id</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso o filme seja atualizado com sucesso</response>
         [HttpPut("{id}")]
         public IActionResult UpdateFilm(int id, [FromBody] UpdateFilmDto filmDto)
         {
@@ -65,6 +93,12 @@ namespace FilmsAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Atualiza o filme parcialmente pelo Id
+        /// </summary>
+        /// <param name="id">Parametros necessários a atualização do filme pelo Id</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso o filme seja atualizado com sucesso</response>
         [HttpPatch("{id}")]
         public IActionResult UpdateFilmPatch(int id, [FromBody] JsonPatchDocument<UpdateFilmDto> patch)
         {
@@ -76,7 +110,7 @@ namespace FilmsAPI.Controllers
             }
 
             var filmToUpdate = _mapper.Map<UpdateFilmDto>(film);
-            patch.ApplyTo(filmToUpdate, ModelState);
+            patch.ApplyTo(filmToUpdate, (Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter)ModelState);
 
             if (!TryValidateModel(filmToUpdate))
             {
@@ -87,6 +121,26 @@ namespace FilmsAPI.Controllers
             _context.SaveChanges();
 
             return NoContent();
+        }
+
+        /// <summary>
+        /// Deleta o filme pelo Id
+        /// </summary>
+        /// <param name="id">Parametros necessários a exclusão do filme pelo Id</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso o filme seja excluído com sucesso</response>
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteFilm(int id)
+        {
+            var film = FindById(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(film);
+            return NoContent();
+
         }
     }
 }
--- FilmsAPI/DTOS/ReadFilmDto.cs
+++ FilmsAPI/DTOS/ReadFilmDto.cs
@@ -2,20 +2,12 @@
 
 namespace FilmsAPI.DTOS
 {
-    public class ReadFilmDto
-    {
-        [Required(ErrorMessage = "Title is obrigatory")]
-        public string Title { get; set; }
-
-        [Required(ErrorMessage = "Description is obrigatory")]
-        public string Description { get; set; }
-
-        [Required(ErrorMessage = "Genre is obrigatory")]
-        [StringLength(50, ErrorMessage = "The size of title exceed 50 characters")]
-        public string Genre { get; set; }
-
-        [Required(ErrorMessage = "Duration is obrigatory")]
-        [Range(70, 600, ErrorMessage = "The duration must be between 70 and 600 minutes")]
+    public class ReadFilmDto    {
+        
+        public string Title { get; set; }        
+        public string Description { get; set; }        
+        public string Genre { get; set; }        
         public int Duration { get; set; }
+        public DateTime Hour { get; set; } = DateTime.Now;
     }
 }
